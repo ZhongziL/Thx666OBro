4162012e23dc762f8348cf413bdb0b2b7f96c39b
JeremieCHN <274488038@qq.com>
2017-06-04 17:21:09 +0800

    视频播放功能完成

--- FrontEnd/movielist/play-video.js
+++ FrontEnd/movielist/play-video.js
@@ -4,16 +4,20 @@ function play_video() {
     }
 
     $(".play").each(function(index, button) {
-        src = getVideoSrc($(button).parents("li").children('message').children('.name').html());
         $(button).click(function(event) {
-            $("#play-video p").after("<embed src='"+ src
-                +"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
             $("#play-video").css('visibility', 'visible');
             $("body").css({
                 'height': '100vh',
                 'overflow-y': 'hidden',
                 'overflow-x': 'scroll'
             });
+
+            movie_name = $(button).parents("li").children('.message').children('.name').html();
+            // TODO: URL需补全
+            // $.get('?moviename=' + movie_name, function(data) {
+            //     $("#play-video p").after("<embed src='"+ data
+            //         +"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
+            // });
         });
     });
     $("#play-video > p").click(function(event) {
