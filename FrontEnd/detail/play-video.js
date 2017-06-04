f1b94e7212beba3f76e84f1b45134a32f3e305dc
JeremieCHN <274488038@qq.com>
2017-06-04 17:46:53 +0800

    视频功能部分完成

--- FrontEnd/detail/play-video.js
+++ FrontEnd/detail/play-video.js
@@ -1,14 +1,27 @@
-function play_video(video_src) {
+function play_video() {
+    movie_name = $("#movie-name").html();
+    movie_src = "";
+
     $("#play").click(function(event) {
-        console.log("ri");
-        $("#play-video p").after("<embed src='"+video_src+"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
         $("#play-video").css('visibility', 'visible');
         $("body").css({
             'height': '100vh',
             'overflow-y': 'hidden',
             'overflow-x': 'scroll'
         });
+
+        if (movie_src == "") {
+            // TODO: 视频功能，URL需补全
+            // $.get('?moviename=' + movie_name, function(data) {
+            //     $("#play-video p").after("<embed src='"+ data
+            //         +"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
+            // });
+        } else {
+            // $("#play-video p").after("<embed src='"+ movie_src
+            //     +"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
+        }
     });
+
     $("#play-video > p").click(function(event) {
         $("#play-video").css('visibility', 'hidden');
         $("#play-video embed").remove();
