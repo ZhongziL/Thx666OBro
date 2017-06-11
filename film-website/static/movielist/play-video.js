23ff6d37127faf312e3d9b4a965a66ed0df49589
JeremieCHN <274488038@qq.com>
2017-06-11 22:05:02 +0800

    movie list

--- film-website/static/movielist/play-video.js
+++ film-website/static/movielist/play-video.js
@@ -1,23 +1,16 @@
 function play_video() {
-    $(".play").each(function(index, button) {
-        $(button).click(function(event) {
+    $(".play").each(function (index, button) {
+        $(button).click(function (event) {
             $("#play-video").css('visibility', 'visible');
             $("body").css({
                 'height': '100vh',
                 'overflow-y': 'hidden',
                 'overflow-x': 'scroll'
             });
-
-            movie_name = $(button).parents("li").children('.message').children('.name').html();
-            // TODO: URL需补全
-            // $.get('?moviename=' + movie_name, function(data) {
-            //     if ($("#play-video").css('visibility') == 'visible')
-            //         $("#play-video p").after("<embed src='"+ data
-            //             +"' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
-            // });
+            $("#play-video p").after("<embed src='" + $(button).siblings(".video-url").html() + "' allowFullScreen='true' quality='high' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>");
         });
     });
-    $("#play-video > p").click(function(event) {
+    $("#play-video > p").click(function (event) {
         $("#play-video").css('visibility', 'hidden');
         $("#play-video embed").remove();
         $("body").css("height", "auto");
